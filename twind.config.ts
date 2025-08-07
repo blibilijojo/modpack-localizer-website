a001b878f275a40735bce1707b0f56c3a5ca773c
blibilijojo <102420734+blibilijojo@users.noreply.github.com>
2025-08-08 05:16:20 +0800

    Update twind.config.ts

--- twind.config.ts
+++ twind.config.ts
@@ -2,4 +2,29 @@ import { Options } from "$fresh/plugins/twind.ts";
 
 export default {
   selfURL: import.meta.url,
+  theme: {
+    extend: {
+      keyframes: {
+        'fade-in-down': {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(-20px)'
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateY(0)'
+          },
+        },
+        'fade-in': {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        }
+      },
+      animation: {
+        'fade-in-down': 'fade-in-down 0.6s ease-out forwards',
+        'fade-in-slow': 'fade-in 1s ease-out forwards',
+        'fade-in': 'fade-in 0.5s ease-out forwards',
+      }
+    }
+  }
 } as Options;
