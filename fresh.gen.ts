97ec7f4842e31e4df581f45d7d7cd55ed8a734fe
blibilijojo <102420734+blibilijojo@users.noreply.github.com>
2025-08-08 00:21:18 +0800

    Update fresh.gen.ts

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -1,20 +1,14 @@
-import * as $0 from "./routes/_layout.tsx";
-import * as $1 from "./routes/index.tsx";
-import * as $$0 from "./islands/Footer.tsx";
-import * as $$1 from "./islands/Header.tsx";
-import * as $$2 from "./islands/HomePage.tsx";
-import * as $$3 from "./islands/LanguageSwitcher.tsx";
+import * as $0 from "./routes/index.tsx";
+import * as $$0 from "./islands/App.tsx";
+import * as $$1 from "./islands/LanguageSwitcher.tsx";
 
 const manifest = {
   routes: {
-    "./routes/_layout.tsx": $0,
-    "./routes/index.tsx": $1,
+    "./routes/index.tsx": $0,
   },
   islands: {
-    "./islands/Footer.tsx": $$0,
-    "./islands/Header.tsx": $$1,
-    "./islands/HomePage.tsx": $$2,
-    "./islands/LanguageSwitcher.tsx": $$3,
+    "./islands/App.tsx": $$0,
+    "./islands/LanguageSwitcher.tsx": $$1,
   },
   baseUrl: import.meta.url,
 };
